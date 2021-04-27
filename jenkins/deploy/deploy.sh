bd14beb01be27111bc3f114083a2981ae2af7f5f
Ubuntu <ubuntu@ip-172-31-11-154.eu-west-1.compute.internal>
2021-04-27 15:58:31 +0000

    Change jenkins key path

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo app > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 #echo $PASS >> /tmp/.auth
 
-sudo scp -i /home/ubuntu/master21.pem /tmp/.auth ubuntu@ec2-3-250-155-176.eu-west-1.compute.amazonaws.com:/tmp/.auth
-sudo scp -i /home/ubuntu/master21.pem ./jenkins/deploy/publish ubuntu@ec2-3-250-155-176.eu-west-1.compute.amazonaws.com:/tmp/publish
+sudo scp -i /opt/aws/master21.pem /tmp/.auth ubuntu@ec2-3-250-155-176.eu-west-1.compute.amazonaws.com:/tmp/.auth
+sudo scp -i /opt/aws/master21.pem ./jenkins/deploy/publish ubuntu@ec2-3-250-155-176.eu-west-1.compute.amazonaws.com:/tmp/publish
 #sudo ssh -i /home/ubuntu/master21.pem  ubuntu@ec2-3-250-155-176.eu-west-1.compute.amazonaws.com && sudo /tmp/publish
